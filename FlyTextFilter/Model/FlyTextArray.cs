d03eba528a46c6d69aca59fc9c7c82a66091f4f9
Aireil <33433913+Aireil@users.noreply.github.com>
2022-06-29 15:40:38 +0200

    chore: image and group clarifications

--- FlyTextFilter/Model/FlyTextArray.cs
+++ FlyTextFilter/Model/FlyTextArray.cs
@@ -8,7 +8,10 @@ public struct FlyTextArray
     [StructLayout(LayoutKind.Explicit, Size = 0x30)]
     public struct FlyTextGroup
     {
-        [FieldOffset(0x00)] public unsafe long* AtkRelatedDataStruct; // **(long**)(*structData + 0x10) == AtkComponentNode
+        // linked list of nodes in addon + 0x238 (+ 0x230 is the first res node)
+        // 54 * 0x48 followed by 76 * 0x48 (static vs dynamic)
+        // first 0x40 bytes of each are for all the nodes of the component node
+        [FieldOffset(0x00)] public unsafe long* LinkedList; // **(long**)(*LinkedList + 0x10) == AtkComponentNode
         [FieldOffset(0x08)] public long CurrentNbOfNodes;
         [FieldOffset(0x10)] public float X;
         [FieldOffset(0x14)] public float Y;
@@ -17,7 +20,7 @@ public struct FlyTextArray
         [FieldOffset(0x20)] public float VerticalOffset;
         [FieldOffset(0x24)] public short MaxNbOfNodes; // will delete the oldest node if this number is reached
         [FieldOffset(0x26)] public short Priority1; // (== 9 - group index) (9->0)
-        [FieldOffset(0x28)] public short Priority2; // Final priority == ((5000 * Priority1) + Priority2)
+        [FieldOffset(0x28)] public short Priority2; // final priority == ((5000 * Priority1) + Priority2)
     }
 
     // [0] on self, healing
