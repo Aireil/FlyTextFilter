e6eb01f684b683db11732dc37968ba12d8d86268
Aireil <33433913+Aireil@users.noreply.github.com>
2022-12-13 23:33:04 +0100

    feat: create fly text when adjusting sliders

--- FlyTextFilter/GUI/AdjustmentsTab.cs
+++ FlyTextFilter/GUI/AdjustmentsTab.cs
@@ -22,41 +22,38 @@ public class AdjustmentsTab
         var posConfig = adjustmentsConfig.FlyTextPositions;
         var defaultPosConfig = FlyTextPositions.GetDefaultPositions();
 
+        var hasPosChanged = false;
         var tmp = posConfig.HealingGroupX ?? defaultPosConfig.HealingGroupX!.Value;
         if (ImGui.DragFloat("Healing horizontal##HealingGroupPosXSlider", ref tmp))
         {
             posConfig.HealingGroupX = tmp;
-            Service.Configuration.Save();
+            hasPosChanged = true;
         }
 
         tmp = posConfig.HealingGroupY ?? defaultPosConfig.HealingGroupY!.Value;
         if (ImGui.DragFloat("Healing vertical##HealingGroupPosYSlider", ref tmp))
         {
             posConfig.HealingGroupY = tmp;
-            Service.Configuration.Save();
+            hasPosChanged = true;
         }
 
         tmp = posConfig.StatusDamageGroupX ?? defaultPosConfig.StatusDamageGroupX!.Value;
         if (ImGui.DragFloat("Status-Damage horizontal##StatusDamageGroupPosXSlider", ref tmp))
         {
             posConfig.StatusDamageGroupX = tmp;
-            Service.Configuration.Save();
+            hasPosChanged = true;
         }
 
         tmp = posConfig.StatusDamageGroupY ?? defaultPosConfig.StatusDamageGroupY!.Value;
         if (ImGui.DragFloat("Status-Damage vertical##StatusDamageGroupPosYSlider", ref tmp))
         {
             posConfig.StatusDamageGroupY = tmp;
-            Service.Configuration.Save();
+            hasPosChanged = true;
         }
 
         if (ImGui.Button("Test##TestPositions"))
         {
-            FlyTextHandler.ApplyPositions();
-
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack2, 0, 0);
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedIcon, 0, 0);
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack, 0, 0);
+            CreateFlyPopupText(true, false);
         }
 
         ImGui.SameLine();
@@ -64,7 +61,14 @@ public class AdjustmentsTab
         {
             FlyTextHandler.ResetPositions();
             Service.Configuration.FlyTextAdjustments.FlyTextPositions = new FlyTextPositions();
+            hasPosChanged = true;
+        }
+
+        if (hasPosChanged)
+        {
             Service.Configuration.Save();
+            FlyTextHandler.ApplyPositions();
+            CreateFlyPopupText(true, false);
         }
 
         ImGui.Unindent();
@@ -78,29 +82,24 @@ public class AdjustmentsTab
         ImGui.Text("Fly texts are from or on you, pop-ups are from or on others." +
                    "\nThere is more involved, but this is a good summary for most.");
 
+        var hasPopUpTextPosChanged = false;
         tmp = adjustmentsConfig.FlyTextScale ?? 1.0f;
         if (ImGui.DragFloat("Fly text scaling##FlyTextScaleSlider", ref tmp, 0.005f))
         {
             adjustmentsConfig.FlyTextScale = tmp;
-            Service.Configuration.Save();
-            FlyTextHandler.ApplyScaling();
+            hasPopUpTextPosChanged = true;
         }
 
         tmp = adjustmentsConfig.PopupTextScale ?? 1.0f;
         if (ImGui.DragFloat("Pop-up text scaling##PopupTextScaleSlider", ref tmp, 0.005f))
         {
             adjustmentsConfig.PopupTextScale = tmp;
-            Service.Configuration.Save();
-            FlyTextHandler.ApplyScaling();
+            hasPopUpTextPosChanged = true;
         }
 
         if (ImGui.Button("Test##TestScaling"))
         {
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack2, 0, 0);
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedIcon, 0, 0);
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack, 0, 0);
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack, 3, 2);
-            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack, 2, 2);
+            CreateFlyPopupText(true, true);
         }
 
         ImGui.SameLine();
@@ -109,7 +108,14 @@ public class AdjustmentsTab
             FlyTextHandler.ResetScaling();
             Service.Configuration.FlyTextAdjustments.FlyTextScale = null;
             Service.Configuration.FlyTextAdjustments.PopupTextScale = null;
+            hasPopUpTextPosChanged = true;
+        }
+
+        if (hasPopUpTextPosChanged)
+        {
             Service.Configuration.Save();
+            FlyTextHandler.ApplyScaling();
+            CreateFlyPopupText(true, true);
         }
 
         ImGui.Unindent();
@@ -126,6 +132,22 @@ public class AdjustmentsTab
             ImGui.TextColored(ImGuiColors.DalamudRed, "Pop-up texts are disabled in the game settings.");
         }
 
-        ImGui.TextColored(ImGuiColors.DalamudGrey, "Tips: ctrl + click to edit values manually, this works in all plugins.");
+        ImGui.TextColored(ImGuiColors.DalamudGrey, "Tip: ctrl + click to directly edit values, this works in all plugins.");
+    }
+
+    public static void CreateFlyPopupText(bool createFlyText, bool createPopUpText)
+    {
+        if (createFlyText)
+        {
+            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack2, 0, 0);
+            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedIcon, 0, 0);
+            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack, 0, 0);
+        }
+
+        if (createPopUpText)
+        {
+            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack, 3, 2);
+            Service.FlyTextHandler.CreateFlyText(FlyTextKind.NamedAttack, 2, 2);
+        }
     }
 }
