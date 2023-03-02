2ab28dfe338d295dc61280ee23e189d388cbeaba
Aireil <33433913+Aireil@users.noreply.github.com>
2023-03-02 20:47:24 +0100

    feat: add an help button for type table

--- FlyTextFilter/GUI/TypesTab/TypesTab.cs
+++ FlyTextFilter/GUI/TypesTab/TypesTab.cs
@@ -59,10 +59,47 @@ public class TypesTab
 
         this.DrawConfirmationImportAllPopup();
 
+        ImGui.SameLine();
+        if (Util.DrawButtonIcon(FontAwesomeIcon.InfoCircle))
+        {
+            ImGui.OpenPopup("##TypeHelp");
+        }
+
+        Util.SetHoverTooltip("Help");
+
+        DrawTypeHelpPopup();
+
         ImGui.AlignTextToFramePadding();
         Service.ConfigWindow.DrawMessage();
 
         this.typesTable.Draw();
+    }
+
+    private static void DrawTypeHelpPopup()
+    {
+        if (ImGui.BeginPopup("##TypeHelp"))
+        {
+            ImGui.Text("The filtering is based on source and target, spread in 3 different groups:");
+            ImGui.Text("(Y)ou - Only from or to you.");
+            ImGui.Text("(P)arty - Only from or to your party members, excluding you.");
+            ImGui.Text("(O)thers - From or to everyone, excluding you and your party members.");
+            ImGui.Text("The main table can be a bit confusing at first. The top part (You - Party - Others) is the source," +
+                       "\nand every letter underneath refers to the target.");
+            ImGui.Text("For example:");
+            ImGui.Text("   - the column Y under You refers to the fly texts from you to you.");
+            ImGui.Text("   - the column P under You refers to the fly texts from you to party members.");
+            ImGui.Text("   - the column Y under Party refers to the fly texts from your party members to you.");
+
+            ImGui.AlignTextToFramePadding();
+            ImGui.Text("If you are still confused, use this button in the table: ");
+
+            ImGui.SameLine();
+            ImGui.PushFont(UiBuilder.IconFont);
+            ImGui.Text(FontAwesomeIcon.Edit.ToIconString());
+            ImGui.PopFont();
+
+            ImGui.EndPopup();
+        }
     }
 
     private static void DrawConfirmationDeleteAllPopup()
